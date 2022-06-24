20412417f4766bbfe3b9cbc446b98f7787aa435e
Felix Nunez G <felixjavier0gmail.com>
2022-06-24 09:43:16 -0500

    Solicitud de Nonato

--- application/views/agro.php
+++ application/views/agro.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html class="no-js" lang="es"> 
+<html class="no-js" lang="es">
+
 <head>
   <meta charset="utf-8">
   <title>Carreras</title>
@@ -8,13 +9,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php $this->load->view('scripts') ?>
 </head>
+
 <body>
   <?php $this->load->view('navigation') ?>
 
   <div class="container section">
     <!-- SECTION TITLE -->
     <div class="row bg-gray section-title">
-      <div class="col-md-12"><h2>AGROBIOTECNOLOGÍA</h2></div>
+      <div class="col-md-12">
+        <h2>AGROBIOTECNOLOGÍA</h2>
+      </div>
     </div>
     <!-- SINGLE -->
     <div class="row">
@@ -42,169 +46,199 @@
           </div>
         </div>
         <div class="row">
+          <div class="col-md-6">
+            <p class="m-top-md text-justify">Objetivos educacionales</p>
+            <ul>
+              <li>El egresado de la Ingeniería en Agrobiotecnología determina las características de bioproductos considerando las necesidades del sector productivo, las características del sistema donde se va a utilizar y factores socioeconómicos, para planear su diseño.</li>
+              <li>El egresado implementa protocolos de obtención de bioproductos a través de su planeación, desarrollo y validación, mediante métodos y técnicas biotecnológicas apegados a la normatividad aplicable, para determinar las especificaciones técnicas del bioproducto.. </li>
+              <li>El egresado valida bioproductos in situ mediante la aplicación y evaluación en los sistemas de producción, apegado a un protocolo de obtención para determinar su efectividad.</li>
+              <li>El egresado planea el proceso de producción agrobiotecnológico considerando especificaciones técnicas del producto y del proceso, los recursos humanos, materiales, financieros y la normatividad aplicable, y establece los indicadores del proceso, para cumplir con los objetivos planteados.</li>
+              <li>El egresado desarrolla el proceso de producción agrobiotecnológico con base en la planeación establecida y
+                aplicación de métodos y técnicas biotecnológicas, así como herramientas de supervisión y control para garantizar la calidad del producto.</li>
+              <li>El egresado evalúa el proceso de producción agrobiotecnológico a través del análisis de los indicadores establecidos y los resultados obtenidos, mediante las herramientas de evaluación y calidad para establecer acciones de mejora y contribuir con la transferencia de tecnología.</li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+          <p class="m-top-md text-justify">Atributos de egreso</p>
+          <ul>
+            <li>Identificar y resolver la problemática agrícola aplicando las bases de la Agrobiotecnología y las Ciencias básicas.</li>
+            <li>Aplicar y analizar diseños de proyectos Agrobiotecnológicos que resuelven necesidades particulares con enfoque de conservación de los recursos naturales.</li>
+            <li>Desarrollar y conducir experimentos, analizar resultados para proponer soluciones a determinadas problemáticas.</li>
+            <li>Fortalecer la competencia de comunicación efectiva en eventos de divulgación agrícola o científica.</li>
+            <li>Poner en práctica sus responsabilidades éticas y profesionales en el desempeño de su profesión y en la asesoría a productores en un contexto global y ambiental.</li>
+            <li>Capacidad para integrar y aplicar sus conocimientos adecuadamente y reconocer la necesidad de capacitación adicional.</li>
+            <li>Capacidad de trabajo en equipo con establecimiento de objetivos claros, metas alcanzables y análisis de riesgos.</li>
+            <li>Aplicar estrategias biotecnológicas en la nutrición de cultivos y en el control de plagas y enfermedades.</li>
+            <li>Elaborar bioproductos que contribuyan con el incremento de la producción agrícola.</li>
+            <li>Aplicar diseños experimentales en la evaluación de productos, métodos y variedades para proponer alternativas más acertadas de tipo sostenible.</li>
+          </ul>
+          </div>
+        </div>
+        <div class="row">
           <div class="col-md-12">
             <p class="m-top-md text-justify">Mapa cuatrimestral</p>
             <div class="table-responsive">
-            <table class="table table-striped table-bordered">
-              <tr>
-                <th colspan="6">TSU en Agrobiotecnología</th>
-                <th colspan="5">Ing. en Agrobiotecnología</th>
-              </tr>
-              <tr>
-                <th>1°</th>
-                <th>2°</th>
-                <th>3°</th>
-                <th>4°</th>
-                <th>5°</th>
-                <th>6°</th>
-                <th>7°</th>
-                <th>8°</th>
-                <th>9°</th>
-                <th>10°</th>
-                <th>11°</th>
-              </tr>
-              <tr>
-                <Td>Algebra Lineal</Td>
-                <Td>Funciones Matemáticas</A></Td>
-                <Td>Cálculo Diferencial</Td>
-                <Td>Cálculo Integral</Td>
-                <Td>Diseños Experimentales</Td>
-                <Td Rowspan="11"><Strong>Estadía</Strong></Td>
-                <Td>Matematicas Para Ingeniería I</Td>
-                <Td>Matematicas Para Ingeniería II</Td>  
-                <Td>Operaciones Unitarias II</Td>
-                <Td>Ingeniería Genética</Td>
-                <Td Rowspan="11"><Strong>Estadía</Strong></Td>
-              </Tr>
-              <Tr>
-                <Td>Química Básica</Td>
-                <Td>Química Orgánica</Td>
-                <Td>Química Analítica</Td>
-                <Td>Probabilidad Y Estadística</Td>
-                <Td>Biorremediación</A></Td>
-                <!-- <Td></Td> -->
-                <Td>Termodinámica</Td>
-                <Td>Bioestadística</Td>
-                <Td>Biología Molecular</Td>
-                <Td>Ingeniería Económica</Td>
-                <!-- <Td></Td> -->
-              </Tr>
-              <Tr>
-                <Td>Biología Agrícola</A></Td>
-                <Td>Física</A></Td>
-                <Td>Electricidad Y Magnetismo</A></Td>
-                <Td>Agricultura Sostenible</A></Td>
-                <Td>Extracción De Metabolitos</A></Td>
-                <!-- <Td></Td> -->
-                <Td>Fisicoquímica</A></Td>
-                <Td>Operaciones Unitarias I</Td>
-                <Td></Td>
-                <Td></Td>
-                <!-- <Td></Td> -->
-              </Tr>
-            </Tr>
-            <Tr>
-              <Td>Informática</Td>
-              <Td>Microbiología</Td>
-              <Td>Genética Vegetal</Td>
-              <Td>Propagación Vegetativa</Td>
-              <Td>Abonos Orgánicos</Td>
-              <!-- <Td></Td> -->
-              <Td>Metodología De La Investigación</A></Td>
-              <Td>Administración De La Calidad</A></Td>
-              <Td>Conservación De Bioproductos</Td>
-              <Td>Caracterización De Bioproductos</Td>
-              <!-- <Td></Td> -->
-            </Tr>
-            <Tr>
-              <Td>Botánica Sistemática</Td>
-              <Td>Bioquímica</Td>
-              <Td>Agrobiotecnología</Td>
-              <Td>Integradora I</Td>
-              <Td>Biofertilizantes</Td>
-              <!-- <Td></Td> -->
-              <Td>Administración De La Producción Agrobiotecnológica</A></Td>
-              <Td>Inglés VII</A></Td>
-              <Td>Bioingeniería</Td>
-              <Td>Integradora</Td>
-              <!-- <Td></Td> -->
-            </Tr>
-            <Tr>
-              <Td>Fisiología Vegetal</A></Td>
-              <Td>Biotecnología Vegetal</Td>
-              <Td>Agrometeorología</Td>
-              <Td>Ecología Microbiana</A></Td>
-              <Td>Control Biológico</A></Td>
-              <!-- <Td></Td> -->
-              <Td>Inglés VI</A></Td>
-              <Td>Planeación Y Organización Del Trabajo</A></Td>
-              <Td>Inglés VIII</Td>
-              <Td>Inglés IX</Td>
-              <!-- <Td></Td> -->
-            </Tr>
-            <Tr>
-              <Td>Inglés I</Td>
-              <Td>Edafología</Td>
-              <Td>Herramientas De Planeación Y Costos</Td>
-              <Td>Fitopatología</Td>
-              <Td>Integradora II</Td>
-              <!-- <Td></Td> -->
-              <Td>Administración Del Tiempo</Td>
-              <Td></Td>
-              <Td>Dirección De Equipos De Alto Rendimiento</Td>
-              <Td>Negociación Empresarial</Td>
-              <!-- <Td></Td> -->
-            </Tr>
-            <Tr>
-              <Td>Expresión Oral Y Escrita I</Td>
-              <Td>Inglés II</Td>
-              <Td>Inglés III</Td>
-              <Td>Control De Plagas Y Malezas</Td>
-              <Td>Inglés V</Td>
-              <!-- <Td></Td> -->
-              <Td></Td>
-              <Td></Td>
-              <Td></Td>
-              <Td></Td>
-              <!-- <Td></Td> -->
-            </Tr>
-            <Tr>
-              <Td></Td>
-              <Td>Formación Sociocultural I</Td>
-              <Td>Formación Sociocultural II</Td>
-              <Td>Inglés IV</Td>
-              <Td>Expresión Oral Y Escrita II</Td>
-              <!-- <Td></Td> -->
-              <Td></Td>
-              <Td></Td>
-              <Td></Td>
-              <Td></Td>
-              <!-- <Td></Td> -->
-            </Tr>
-            <Tr>
-              <Td></Td>
-              <Td></Td>
-              <Td></Td>
-              <Td>Formación Sociocultural III</Td>
-              <Td>Formación Sociocultural IV</Td>
-              <!-- <Td></Td> -->
-              <Td></Td>
-              <Td></Td>
-              <Td></Td>
-              <Td></Td>
-              <!-- <Td></Td> -->
-            </Tr>
+              <table class="table table-striped table-bordered">
+                <tr>
+                  <th colspan="6">TSU en Agrobiotecnología</th>
+                  <th colspan="5">Ing. en Agrobiotecnología</th>
+                </tr>
+                <tr>
+                  <th>1°</th>
+                  <th>2°</th>
+                  <th>3°</th>
+                  <th>4°</th>
+                  <th>5°</th>
+                  <th>6°</th>
+                  <th>7°</th>
+                  <th>8°</th>
+                  <th>9°</th>
+                  <th>10°</th>
+                  <th>11°</th>
+                </tr>
+                <tr>
+                  <Td>Algebra Lineal</Td>
+                  <Td>Funciones Matemáticas</A></Td>
+                  <Td>Cálculo Diferencial</Td>
+                  <Td>Cálculo Integral</Td>
+                  <Td>Diseños Experimentales</Td>
+                  <Td Rowspan="11"><Strong>Estadía</Strong></Td>
+                  <Td>Matematicas Para Ingeniería I</Td>
+                  <Td>Matematicas Para Ingeniería II</Td>
+                  <Td>Operaciones Unitarias II</Td>
+                  <Td>Ingeniería Genética</Td>
+                  <Td Rowspan="11"><Strong>Estadía</Strong></Td>
+                </Tr>
+                <Tr>
+                  <Td>Química Básica</Td>
+                  <Td>Química Orgánica</Td>
+                  <Td>Química Analítica</Td>
+                  <Td>Probabilidad Y Estadística</Td>
+                  <Td>Biorremediación</A></Td>
+                  <!-- <Td></Td> -->
+                  <Td>Termodinámica</Td>
+                  <Td>Bioestadística</Td>
+                  <Td>Biología Molecular</Td>
+                  <Td>Ingeniería Económica</Td>
+                  <!-- <Td></Td> -->
+                </Tr>
+                <Tr>
+                  <Td>Biología Agrícola</A></Td>
+                  <Td>Física</A></Td>
+                  <Td>Electricidad Y Magnetismo</A></Td>
+                  <Td>Agricultura Sostenible</A></Td>
+                  <Td>Extracción De Metabolitos</A></Td>
+                  <!-- <Td></Td> -->
+                  <Td>Fisicoquímica</A></Td>
+                  <Td>Operaciones Unitarias I</Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <!-- <Td></Td> -->
+                </Tr>
+                </Tr>
+                <Tr>
+                  <Td>Informática</Td>
+                  <Td>Microbiología</Td>
+                  <Td>Genética Vegetal</Td>
+                  <Td>Propagación Vegetativa</Td>
+                  <Td>Abonos Orgánicos</Td>
+                  <!-- <Td></Td> -->
+                  <Td>Metodología De La Investigación</A></Td>
+                  <Td>Administración De La Calidad</A></Td>
+                  <Td>Conservación De Bioproductos</Td>
+                  <Td>Caracterización De Bioproductos</Td>
+                  <!-- <Td></Td> -->
+                </Tr>
+                <Tr>
+                  <Td>Botánica Sistemática</Td>
+                  <Td>Bioquímica</Td>
+                  <Td>Agrobiotecnología</Td>
+                  <Td>Integradora I</Td>
+                  <Td>Biofertilizantes</Td>
+                  <!-- <Td></Td> -->
+                  <Td>Administración De La Producción Agrobiotecnológica</A></Td>
+                  <Td>Inglés VII</A></Td>
+                  <Td>Bioingeniería</Td>
+                  <Td>Integradora</Td>
+                  <!-- <Td></Td> -->
+                </Tr>
+                <Tr>
+                  <Td>Fisiología Vegetal</A></Td>
+                  <Td>Biotecnología Vegetal</Td>
+                  <Td>Agrometeorología</Td>
+                  <Td>Ecología Microbiana</A></Td>
+                  <Td>Control Biológico</A></Td>
+                  <!-- <Td></Td> -->
+                  <Td>Inglés VI</A></Td>
+                  <Td>Planeación Y Organización Del Trabajo</A></Td>
+                  <Td>Inglés VIII</Td>
+                  <Td>Inglés IX</Td>
+                  <!-- <Td></Td> -->
+                </Tr>
+                <Tr>
+                  <Td>Inglés I</Td>
+                  <Td>Edafología</Td>
+                  <Td>Herramientas De Planeación Y Costos</Td>
+                  <Td>Fitopatología</Td>
+                  <Td>Integradora II</Td>
+                  <!-- <Td></Td> -->
+                  <Td>Administración Del Tiempo</Td>
+                  <Td></Td>
+                  <Td>Dirección De Equipos De Alto Rendimiento</Td>
+                  <Td>Negociación Empresarial</Td>
+                  <!-- <Td></Td> -->
+                </Tr>
+                <Tr>
+                  <Td>Expresión Oral Y Escrita I</Td>
+                  <Td>Inglés II</Td>
+                  <Td>Inglés III</Td>
+                  <Td>Control De Plagas Y Malezas</Td>
+                  <Td>Inglés V</Td>
+                  <!-- <Td></Td> -->
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <!-- <Td></Td> -->
+                </Tr>
+                <Tr>
+                  <Td></Td>
+                  <Td>Formación Sociocultural I</Td>
+                  <Td>Formación Sociocultural II</Td>
+                  <Td>Inglés IV</Td>
+                  <Td>Expresión Oral Y Escrita II</Td>
+                  <!-- <Td></Td> -->
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <!-- <Td></Td> -->
+                </Tr>
+                <Tr>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td>Formación Sociocultural III</Td>
+                  <Td>Formación Sociocultural IV</Td>
+                  <!-- <Td></Td> -->
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <!-- <Td></Td> -->
+                </Tr>
 
-          </table>
-        </div>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 
 
-<?php $this->load->view('footer'); ?>
-<?php $this->load->view('jscripts'); ?>
+  <?php $this->load->view('footer'); ?>
+  <?php $this->load->view('jscripts'); ?>
 </body>
+
 </html>
