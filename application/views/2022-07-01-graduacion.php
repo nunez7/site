3b925a67c67f61dfe28c616b5403ccfa62f0d02f
Felix Nunez G <felixjavier0@gmail.com>
2022-07-01 16:14:22 -0500

    Nota de Costa

---
+++ application/views/2022-07-01-graduacion.php
@@ -0,0 +1,16 @@
+<p>
+<b>Egresan 108 jóvenes los niveles de Licenciatura e Ingenierías.</b>
+</p>
+
+<p>El impulso a la educación en Nayarit a sido de las prioridades de la actual administración que encabeza el Dr. Miguel Ángel Navarro Quintero y ha permitido que los índices de egresados de nivel superior aumenten.
+</p>
+<p>
+Debido a las facilidades y apoyos que actualmente existen para cursar una carrera profesional, este viernes 01 de julio egresaron 108 jóvenes de la generación 2022 de las Licenciaturas en Administración, Mercadotecnia y Turismo, así como las Ingenierías en Agrobiotecnología, Procesos Alimentarios y Desarrollo y Gestión de Software. Esto a su vez propició que 21 empresas contratarán a nuestros egresados, promoviendo el desarrollo laboral y económico en la zona norte de Nayarit. 
+</p>
+<p>
+El encargado de dirigir un mensaje a los graduados en representación del Gobernador del Estado Dr. Miguel Ángel Navarro Quintero fue el Lic. Carlos Manuel Ovalle Sánchez, Jefe del Departamento de Educación Superior y la encargada de tomar protesta a los egresados fue la Diputada Any Marilú Porras Bailón.  
+De mano Autoridades Estatales, Municipales y de Nuestro Rector Ing. Hermilo Said López Velarde, los egresados recibieron sus respectivos documentos, asimismo se entregaron reconocimientos a mejores promedios y mejores proyectos de estadía. 
+</p>
+<p>
+Es así como esta Institución Educativa entrega a la sociedad egresados con alto grado de competitividad, capaces de afrontar los retos que el mundo actual les presenta, pero sobre todo con el sello de garantía de la Universidad Tecnológica de la Costa.
+</p>
